2f9f9c094220c1574a4c9f7c69ea5bc8e118146e
taezaza-chai <taezazachai@gmail.com>
2025-04-28 10:06:12 +0700

    4

--- Assets/Scripts/Projectil2D.cs
+++ Assets/Scripts/Projectil2D.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Projectil2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target; 
+    [SerializeField] Rigidbody2D bulletPrefab;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit " + hit.collider.name);
+            }
+        }
     }
 }
